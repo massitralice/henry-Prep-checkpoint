b6c2d0d2660e4f8558bf9dd51f45904c31cffc3a
Massimiliano Tralice <massimilianotralice@gmail.com>
2020-05-09 13:21:09 -0300

    Check

--- checkpoint.js
+++ checkpoint.js
@@ -5,8 +5,25 @@ function menorMayor(numeros) {
   // Escribi una función llamada menorMayor que tome como entrada un arreglo de números y devuelva un arreglo 
   // que contenga el menor número del arreglo en la posición cero y el mayor número del arreglo en la posición 1.
   // Ej: menorMayor([4, 6, 1, 7, 15]) // retorna [1, 15]
+ var mayor = numeros[0];
+ var menor = numeros[0];
 
-}
+ for(var i = 0; i < numeros.length; i++){
+    if(mayor < numeros[i]){
+    mayor = numeros[i];
+      }
+  else if(menor > numeros[i]){
+    menor = numeros[i]
+  }
+ }
+ var arrayNuevo = [0,0]
+ arrayNuevo[0] = menor;
+ arrayNuevo[1] = mayor;
+  
+ return arrayNuevo;
+  }
+ 
+
 
 function stringMasLarga(strings) {
   // Escribe una función llamada stringMasLarga, que tome un arreglo de strings llamado 'strings'
@@ -14,7 +31,13 @@ function stringMasLarga(strings) {
   // Ej:
   // stringMasLarga(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
-
+  var comp = "";
+  for(var i = 0; i < strings.length; i++){
+    if(comp.length < strings[i].length){
+      comp = strings[i]
+    }
+  }
+return comp;
 }
 
 function buscarAmigo(amigos, nombre) {
@@ -24,6 +47,11 @@ function buscarAmigo(amigos, nombre) {
   // Ej:
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') // retorna { nombre: 'toni', edad: 33 };
+  for(var i = 0; i < amigos.length; i++){
+    if(nombre === amigos[i].nombre){
+      return amigos[i]
+    }
+  }
   
 }
 
@@ -35,8 +63,15 @@ function sumArray(array, n) {
   // sumArray([2,5,7,10,11,15,20], 13)  // retorna true     2+11 suman 13
   // sumArray([2,5,7,10,11,15,20], 14)  // retorna false
   // pista: podes usar bucles anidados;
-  
-};
+ for(var i = 0; i < array.length; i++){
+   for(var j = i + 1; j < array.length ; j++){
+     if(array[i] + array[j] === n){
+       return true;
+     }
+        }
+   }
+    return false;
+ }
 
 function pluck(array, propiedad) { 
   // Escribi una función llamada pluck,
@@ -46,8 +81,13 @@ function pluck(array, propiedad) {
   // var productos = [{ name: 'TV LCD', price: 100}, { name: 'Computadora', price: 500 }]
   // productos.pluck(productos, 'name') // ['TV LCD', 'Computadora']
   // pista: es una buena oportunidad para usar map.
-  
-  
+ var productosArray = [];
+ for(var i = 0; i < array.length; i++){
+   productosArray.push(array[i][propiedad]);
+ }
+ return productosArray;
+
+
 }
 
 // =======================================================================
@@ -61,6 +101,10 @@ function crearClasePersona() {
 
   class Persona {
     constructor(nombre, edad, hobbies, amigos) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
       
     }
 
@@ -68,20 +112,24 @@ function crearClasePersona() {
       // el metodo addFriend recibe un string nombre y un entero edad y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // no debe retornar nada.
+     
       
     }
 
     addHobby(hobby) {
       // este método debe agregar un hobby (hobby) al arreglo de hobbies de la persona.
       // no debe retornar nada.
-      
+     this.hobbies.push(hobby);
     }
     getFriends() {
       // Escribe una función que retorne un arreglo con sólo los nombres del arreglo de amigos
       // de la persona.
       // Ej:
       // persona.getFriends() // retorna ['toni', 'Leo', 'Manu']
-      
+      var arrayAmigos = []
+      for(var i = 0; i < this.amigos.length; i++){
+        arrayAmigos.push(this.amigos)
+      }
     }
 
     getHobbies() {
--- teoria.md
+++ teoria.md
@@ -1,24 +1,25 @@
 ## Explica los siguentes conceptos con tus propias palabras. (no más de tres líneas por respuesta).
 
-* HTML:
+* HTML: son las reglas o formas de escribir y estructurar el contenido de una pagina web. En si no es un lenguaje de programacion,
+sino que mediante etiquetas indica a un navegador la manera de mostrar cierto contenido.
 
-* CSS:
+* CSS: ya que un html por si mismo solo nos muestra texto plano, lo que dificulta su lectura. CSS nos provee las herramientas para estilizar nuestra pagina web y asi hacerla mas estetica y organizada visualmente.
 
-* Git:
+* Git: es un sistema de control de version, que nos permite clonar repositorios y actualizarlos sin riesgo de modificar o perder el repositorio original, ya que alojamos una copia local donde realizamos las modificaciones.
 
-* Arreglos:
+* Arreglos: los arreglos son una manera de agrupar datos dentro de una misma variable, lo que nos permite seguirlas o modificarlas en "grupo". se escriben como una variable pero dentro de []
 
-* Objetos:
+* Objetos: los objetos tambien son datos agrupados, pero a diferencia de los arreglos en los objetos la idea es agrupar datos de una misma cosa. Se escriben dentro de {}
 
-* Variables:
+* Variables: las variables son, tambien, contenedores de datos donde "guardamos" esos datos para utilizarlos en otro momento. Se escriben con la palabra clave "var", seguida del nombre de la variable, un signo = y el dato que queremos almacenar en ella
 
-* Métodos:
+* Métodos: los metodos son funciones asociadas a un objeto, de forma que nos permite interactuar ese objeto a partir de una sola funcion, lo que de otra manera nos costaria varias funciones.
 
-* DOM:
+* DOM: el modelo de objetos del documento, es la manera en la que se estructuran todos los objetos que forman una web
 
-* Atributo de un tag HTML:
+* Atributo de un tag HTML: el atributo es una clave par:valor de un tag html. Este nos sirve para brindar mas informacion, ya que algunas etiquetas por si solas no tienen una funcion. Por ejemplo el "src" de una etiqueta <img>
 
-* Selector CSS:
+* Selector CSS: el selector de CSS es como la "etiqueta" que, dentro de una hoja de estilos, selecciona a que etiqueta propiamente dicha del html nos estamos refiriendo para darle el estilo que definamos 
 
-* Un función constructora:
+* Un función constructora: es una de la formas de crear clases, a partir de una funcion que por convencion se escribe con la primera letra mayuscula.
 
